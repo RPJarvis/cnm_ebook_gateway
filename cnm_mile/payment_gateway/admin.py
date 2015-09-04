37ab3ba1280032f4ba984e326b98676100adc048
JARVIS <RJarvis1@cnm.edu>
2015-09-04 15:28:19 -0600

    basic admin working, added a product

--- cnm_mile/payment_gateway/admin.py
+++ cnm_mile/payment_gateway/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Product
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['title', 'author', 'price', 'inkling_product_id', 'cover_image']
+
+admin.site.register(Product, ProductAdmin)
