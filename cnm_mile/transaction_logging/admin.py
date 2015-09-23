8ebc2ad9f49cf731e348a2759c39bacfdca78832
JARVIS <RJarvis1@cnm.edu>
2015-09-23 11:02:08 -0600

    customized admin options

--- cnm_mile/transaction_logging/admin.py
+++ cnm_mile/transaction_logging/admin.py
@@ -3,18 +3,29 @@ from .models import TouchnetTransaction, InklingTransaction
 
 
 class TouchnetTransactionAdmin(admin.ModelAdmin):
-    readonly_fields = ['user_id', 'title', 'amount', 'success_or_fail', 'details']
+    readonly_fields = ['user_id', 'first_name', 'last_name', 'title', 'amount', 'success_or_fail', 'details']
 
     #fields = ['user_id', 'title', 'amount', 'success_or_fail', 'details']
-    list_display = ('date_created', 'user_id', 'title', 'amount', 'success_or_fail', 'details')
-    search_fields = ['date_created', 'user_id']
+    list_display = ('date_created', 'user_id', 'first_name', 'last_name', 'title', 'amount', 'success_or_fail', 'details')
+    search_fields = ['date_created', 'user_id', 'last_name']
 
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 class InklingTransactionAdmin(admin.ModelAdmin):
     readonly_fields = fields = ['user_id', 'first_name', 'last_name', 'title', 'success_or_fail', 'details']
    # fields = ['user_id', 'title', 'success_or_fail', 'details']
     list_display = ('date_created', 'user_id', 'first_name', 'last_name', 'title', 'success_or_fail', 'details')
-    search_fields = ['date_created', 'user_id']
+    search_fields = ['date_created', 'user_id', 'last_name']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 admin.site.register(TouchnetTransaction, TouchnetTransactionAdmin)
 admin.site.register(InklingTransaction, InklingTransactionAdmin)
