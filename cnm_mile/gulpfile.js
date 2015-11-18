37721abbcfd53e4ad4aea6f1705b0ac50e6008e1
eugenehiggins <genotype@gknow.com>
2015-11-18 10:03:20 -0700

    development of form block, added autoprefix to the gulp task

--- cnm_mile/gulpfile.js
+++ cnm_mile/gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
     gulp.src('templates/styles.scss')
+    	// .pipe (sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        // .pipe(sourcemaps.write())
+    	.pipe(autoprefixer())
         .pipe(gulp.dest('static/css/'))
 });
 
